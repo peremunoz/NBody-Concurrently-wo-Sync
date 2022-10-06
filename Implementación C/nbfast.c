e0ef99b79f0f1ea9496c800bbe29349a29498e6e
peremunoz <73690199+peremunoz@users.noreply.github.com>
2022-10-06 20:19:19 +0200

    created and assign variable M from argument

--- Implementación C/nbfast.c
+++ Implementación C/nbfast.c
@@ -310,6 +310,7 @@ void ShowWritePartialResults(int count,int nOriginal, int nShared, int *indexes,
 int main(int argc, char *argv[]){
     int nShared=500;
 	int steps=100;
+    int M=4;
     double *sharedBuff;
     double *localBuff;
     double *radius;
@@ -324,6 +325,9 @@ int main(int argc, char *argv[]){
 		nShared=atoi(argv[1]);
 		if(argc>2){
 			steps=atoi(argv[2]);
+            if(argc>4){
+                M=atoi(argv[4]);
+            }
 		}
 	}
 
