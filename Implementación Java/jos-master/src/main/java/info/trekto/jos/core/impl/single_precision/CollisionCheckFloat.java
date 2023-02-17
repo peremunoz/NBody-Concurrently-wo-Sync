056af1ecc183ae3b578b8b7a5b101fae5e7a0f1c
Pere Muñoz <73690199+peremunoz@users.noreply.github.com>
2023-02-17 20:18:35 +0100

    Update CollisionCheckFloat.java

--- Implementación Java/jos-master/src/main/java/info/trekto/jos/core/impl/single_precision/CollisionCheckFloat.java
+++ Implementación Java/jos-master/src/main/java/info/trekto/jos/core/impl/single_precision/CollisionCheckFloat.java
@@ -2,7 +2,7 @@
 Práctica 2.
 Código fuente: CollisionCheckFloat.java
 Grau Informàtica
-48252062V - Pere Muñoz Figuerol
+Pere Muñoz Figuerol
 */
 
 package info.trekto.jos.core.impl.single_precision;
